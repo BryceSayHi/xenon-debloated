555cd392c7239b8797e6b37bfee8a98450535fdb
Bike <140366581+BryceSayHi@users.noreply.github.com>
2026-09-08 16:55:54 -0400

    color rahhh

--- src/main/java/org/embeddedt/embeddium/gui/theme/DefaultColors.java
+++ src/main/java/org/embeddedt/embeddium/gui/theme/DefaultColors.java
@@ -1,5 +1,5 @@
 package org.embeddedt.embeddium.gui.theme;
 
 public class DefaultColors {
-    public static final int ELEMENT_ACTIVATED = 0xFF94E4D3;
+    public static final int ELEMENT_ACTIVATED = 0xFFCF7CB6;
 }
